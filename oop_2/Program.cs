c18bf9c981717df93f902eaa9ace335c7dc86a27
NadaFayez13 <naditafayez13@gmail.com>
2026-09-23 08:20:30 +0300

    oop1_question 1

--- oop_2/Program.cs
+++ oop_2/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace oop_2
+﻿using System;
+
+namespace oop_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region assignement 1 _ q1
+            DeliveryAddress address1 = new DeliveryAddress("Alex", "Gamal Abdelnasser", 13);
+            DeliveryAddress address2 = address1;
+
+            address2.City = "aswan";
+            address2.BuildingNumber = 1;
+
+            Console.WriteLine($"Address 1 (Original): {address1.GetFullAddress()}");
+            Console.WriteLine($"Address 2 (Copied):   {address2.GetFullAddress()}");
+            #endregion
         }
     }
 }
