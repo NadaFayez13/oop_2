c78bb5d6f5c5492c06365058189d88f547c44ee0
NadaFayez13 <naditafayez13@gmail.com>
2026-09-23 08:32:49 +0300

    oop1 _ question 2.1

---
+++ oop_2/Shipment.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace oop_1
+{
+    public struct Shipment
+    {
+        private string trackingCode;
+        private string description;
+        private double weight;
+        private decimal deliveryFee;
+
+        public string TrackingCode
+        {
+            get { return trackingCode; }
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    trackingCode = value;
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    description = value;
+                }
+            }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+            }
+        }
+
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+            private set
+            {
+                if (value > 0)
+                {
+                    deliveryFee = value;
+                }
+            }
+        }
+
+        public DeliveryAddress Destination { get; set; }
+        public decimal EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + ((decimal)Weight * 5m);
+            }
+        }
+    }
+}
